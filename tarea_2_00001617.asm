f65bb2512b5382fdcb04c643b4def5575fe3ce6e
Jose Segura <00001617@uca.edu.sv>
2020-04-25 23:36:03 -0600

    ejercicio 3 creo que ya esta

--- tarea_2_00001617.asm
+++ tarea_2_00001617.asm
@@ -1,4 +1,4 @@
-;---------------Ejercicio 1
+;---------------Ejercicio 1-------------------------
 	org	100h
 	 
 	mov	ax,0d
@@ -31,10 +31,25 @@ lupi: 	mov	cl,"H"
 	mov	cl,"d"
 	mov	[208h],cl
 
-;---------------Ejercicio 2
-	mov 	ax,0d
-	mov 	bx,1d
-	mov	cx,14d
+;---------------Ejercicio 3------------------------------------
+	org 100h 
 	
+	mov	bx, 0d
+	mov	dx, 1d
+	mov	cx, 13d
+	mov 	di, 0d
 
-	int 	20h
+        mov 	[220h], bx
+        inc 	di
+        mov 	[di+220h], dx
+        inc 	di
+holi:	mov	ax, bx
+	add	ax, dx
+        mov 	[di+220h], ax
+        inc 	di
+	mov	bx, dx
+	mov	dx, ax
+
+	Loop 	holi
+
+	int	20h
